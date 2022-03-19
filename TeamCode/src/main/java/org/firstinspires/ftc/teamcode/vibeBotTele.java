ee476540ec2c2feb0c548c40d35d5f4189cfec6a
jai <jaipatel8@outlook.com>
2022-03-18 21:53:18 -0500

    changed vibeBotTele.java to have joystick controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vibeBotTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vibeBotTele.java
@@ -1,10 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import static com.qualcomm.robotcore.util.Range.clip;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name = "Vibe Bot TeleOp")
 public class vibeBotTele extends LinearOpMode {
@@ -16,13 +15,8 @@ public class vibeBotTele extends LinearOpMode {
         waitForStart();
 
         while(!isStopRequested()){
-            if(gamepad1.right_bumper){
-                bot.mallet1.setPosition(1);
-            } else {
-                bot.mallet1.setPosition(.6);
-            }
+            bot.mallet1.setPosition(clip(.60-gamepad1.right_stick_y,.6,1));
             bot.cart1.setVelocity(1000 * (gamepad1.left_trigger- gamepad1.right_trigger));
-
         }
     }
 }
