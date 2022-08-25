ef8f8463be964ef6eceec572c0eb744c119970e7
jai <jaipatel8@outlook.com>
2022-08-24 22:06:32 -0500

    new motor init process

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPracticeJai.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPracticeJai.java
@@ -9,14 +9,15 @@ public class MotorPracticeJai extends OpMode {
     DcMotorEx frontLeft;
     DcMotorEx backRight;
     DcMotorEx backLeft;
-    private final DcMotorEx[] motors = {frontLeft, frontRight, backLeft, backRight};
+    //private final DcMotorEx[] motors = {frontLeft, frontRight, backLeft, backRight};
+    private final DcMotorEx[] noahThing = new DcMotorEx[4];
     private final String[] motorsName = {"frontLeft", "frontRight", "backLeft", "backRight"};
     @Override
     public void init() {
 
-        for (int i = 0; i < motors.length; i++ ){
-            motors[i] = hardwareMap.get(DcMotorEx.class, motorsName[i]);
-            motors[i].setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        for (int i = 0; i < noahThing.length; i++ ){
+            noahThing[i] = hardwareMap.get(DcMotorEx.class, motorsName[i]);
+            noahThing[i].setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         }
     }
 
